1d5d2f1c82b289c5af418be266a47cd39475cd90
emispheal <emispheal@gmail.com>
2024-06-24 15:34:30 +1000

    debugging script error

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,5 @@
 echo "Running container..."
+sudo docker ps -q
+
+docker login --username AWS --password $(aws ecr get-login-password --region ap-southeast-2) 519948377493.dkr.ecr.ap-southeast-2.amazonaws.com
 docker run --name app-ecr-v1 -d -p 5000:5000 519948377493.dkr.ecr.ap-southeast-2.amazonaws.com/app-ecr-v1:latest
