ad7aef8608ff0d2da824d9c51ca311cde361315b
emispheal <emispheal@gmail.com>
2024-06-24 15:27:46 +1000

    fixed script error again wrong name

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,2 @@
 echo "Running container..."
-docker run --name flask_app -d -p 5000:5000 519948377493.dkr.ecr.ap-southeast-2.amazonaws.com/app-ecr-v1:latest
+docker run --name app-ecr-v1 -d -p 5000:5000 519948377493.dkr.ecr.ap-southeast-2.amazonaws.com/app-ecr-v1:latest
